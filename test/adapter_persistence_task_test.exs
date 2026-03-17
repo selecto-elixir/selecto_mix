7cde6d6ab2366920ceed59b04a5a6dd12bd214ff
Chris Rohlfs <seeken@gmail.com>
2026-03-17 11:26:45 -0600

    add filter set task smoke tests

--- test/adapter_persistence_task_test.exs
+++ test/adapter_persistence_task_test.exs
@@ -28,4 +28,56 @@ defmodule SelectoMix.AdapterPersistenceTaskTest do
     assert output =~ "priv/sql/create_saved_view_configs.sql"
     assert output =~ "lib/tmp_app/saved_view_config_context.ex"
   end
+
+  test "filter_sets task generates SQLite raw persistence files" do
+    in_tmp_dir("selecto_mix_filter_sets_sqlite", fn ->
+      Mix.Task.reenable("selecto.gen.filter_sets")
+      Mix.Tasks.Selecto.Gen.FilterSets.run(["TmpApp", "--adapter", "sqlite", "--no-tests"])
+
+      sql_path = Path.join(["priv", "sql", "create_filter_sets.sql"])
+      context_path = Path.join(["lib", "tmp_app", "tmp_app", "filter_sets.ex"])
+
+      sql = File.read!(sql_path)
+      context = File.read!(context_path)
+
+      assert sql =~ "CREATE TABLE IF NOT EXISTS filter_sets"
+      assert sql =~ "filters TEXT NOT NULL DEFAULT '{}'"
+      assert context =~ "SelectoDBSQLite.Adapter"
+
+      refute File.exists?(
+               Path.join(["lib", "tmp_app", "tmp_app", "filter_sets", "filter_set.ex"])
+             )
+    end)
+  end
+
+  test "filter_sets task generates PostgreSQL raw persistence files" do
+    in_tmp_dir("selecto_mix_filter_sets_postgresql", fn ->
+      Mix.Task.reenable("selecto.gen.filter_sets")
+      Mix.Tasks.Selecto.Gen.FilterSets.run(["TmpApp", "--adapter", "postgresql", "--no-tests"])
+
+      sql_path = Path.join(["priv", "sql", "create_filter_sets.sql"])
+      context_path = Path.join(["lib", "tmp_app", "tmp_app", "filter_sets.ex"])
+
+      sql = File.read!(sql_path)
+      context = File.read!(context_path)
+
+      assert sql =~ "filters JSONB NOT NULL DEFAULT '{}'"
+      assert sql =~ "NOW()"
+      assert context =~ "SelectoDBPostgreSQL.Adapter"
+    end)
+  end
+
+  defp in_tmp_dir(prefix, fun) do
+    base_tmp = System.tmp_dir!()
+    tmp_dir = Path.join(base_tmp, "#{prefix}_#{System.unique_integer([:positive])}")
+
+    File.rm_rf!(tmp_dir)
+    File.mkdir_p!(tmp_dir)
+
+    try do
+      File.cd!(tmp_dir, fun)
+    after
+      File.rm_rf!(tmp_dir)
+    end
+  end
 end
